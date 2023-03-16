116d467f46dc606e350c5b139eeea8868d1db516
ardit <arditbderveni@gmail.com>
2023-03-16 19:32:02 +0100

    Tes

--- src/Components/Notes/SingleNote/SingleNote.jsx
+++ src/Components/Notes/SingleNote/SingleNote.jsx
@@ -4,7 +4,7 @@ import classes from "./SIngleNote.module.css";
 const SingleNote = (props) => {
   return (
     <li className={classes.note}>
-      <h3>{props.title}</h3>
+      <h3>{props.title} </h3>
       <div className={classes.content}>{props.content}</div>
       <div className={classes.tag}>{props.tag}</div>
     </li>
